08f9ebd4d4221d90be4294be6e388d18e998d3c5
Jonathan Smith <47522338+Jtsmith275@users.noreply.github.com>
2021-04-21 10:42:33 +0100

    Update rstudioshinynginxwrapper.sh

--- rstudioshinynginxwrapper.sh
+++ rstudioshinynginxwrapper.sh
@@ -35,7 +35,7 @@ sudo apt-get update && sudo apt-get install -y \
     libxml2-dev 	
     
 sudo apt-get update \
-  && sudo apt-get install -y --no-install-recommends \
+  && sudo apt-get install -y  \
     lbzip2 \
     libfftw3-dev \
     libgdal-dev \
