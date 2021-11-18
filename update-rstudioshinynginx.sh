33d03e0620c7ed7048b70217a75b47fd90a153d7
Jonathan Smith <47522338+Jtsmith275@users.noreply.github.com>
2021-11-18 11:53:39 +0000

    Update update-rstudioshinynginx.sh

--- update-rstudioshinynginx.sh
+++ update-rstudioshinynginx.sh
@@ -6,9 +6,9 @@ sudo apt-get update && sudo apt-get install r-base r-base-dev -y
 
 # Install latest verion of RStudio Server
 sudo apt-get install gdebi-core -y
-wget https://www.rstudio.org/download/latest/stable/server/bionic/rstudio-server-latest-amd64.deb
-sudo gdebi --non-interactive rstudio-server-latest-amd64.deb
-rm rstudio-server-latest-amd64.deb
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2021.09.1-372-amd64.deb
+sudo gdebi --non-interactive wget rstudio-server-2021.09.1-372-amd64.deb
+rm  rstudio-server-2021.09.1-372-amd64.deb
 
 # Install nginx this will update it if there is a new version
 sudo apt-get install nginx -y
