aac56d41e25eade0a0fdccc468cca284da6b5232
Jonathan Smith <47522338+Jtsmith275@users.noreply.github.com>
2022-07-14 14:25:11 +0100

    Update rstudioshinynginxwrapper.sh

--- rstudioshinynginxwrapper.sh
+++ rstudioshinynginxwrapper.sh
@@ -67,7 +67,7 @@ sudo apt-get update && sudo apt-get install \
 # add addition system dependencies but suffixing \ <package name> on the end of the apt-get update & apt-get install -y command
 
 # Install Shiny R package
-mkdir -p ~/R/x86_64-pc-linux-gnu-library/4.1
+mkdir -p ~/R/x86_64-pc-linux-gnu-library/4.2
 R -e "install.packages('shiny', repos='https://cran.rstudio.com/', lib='~/R/x86_64-pc-linux-gnu-library/4.1')"
 
 #Install other common R packages
