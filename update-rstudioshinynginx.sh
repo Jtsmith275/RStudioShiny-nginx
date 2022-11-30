a43487898d95ef44597d0192ba8778b295d61090
Jason Bramwell <jb2cool@gmail.com>
2022-11-30 13:08:07 +0000

    Update update-rstudioshinynginx.sh

--- update-rstudioshinynginx.sh
+++ update-rstudioshinynginx.sh
@@ -15,7 +15,7 @@ sudo apt-get install nginx -y
 
 # Install latest version of Shiny Server
 sudo apt-get install curl -y
-VERSION=$(curl https://download3.rstudio.org/ubuntu-14.04/x86_64/VERSION)
-wget --no-verbose "https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-$VERSION-amd64.deb" -O shiny-server-latest.deb
+VERSION=$(curl https://download3.rstudio.org/ubuntu-18.04/x86_64/VERSION)
+wget --no-verbose "https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-$VERSION-amd64.deb" -O shiny-server-latest.deb
 sudo gdebi -n shiny-server-latest.deb
 rm shiny-server-latest.deb
