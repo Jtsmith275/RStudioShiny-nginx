19275c5b3d65c6b1ef40c6640e5bc976685293e6
Jonathan Smith <47522338+Jtsmith275@users.noreply.github.com>
2024-11-02 17:27:07 +0000

    Update rstudioshinynginxwrapper.sh

--- rstudioshinynginxwrapper.sh
+++ rstudioshinynginxwrapper.sh
@@ -1,99 +1,92 @@
 #!/bin/bash
-# R/RStudio/Shiny-Server/nginx on Ubuntu
+# Install R/RStudio Server/Shiny Server/nginx on Ubuntu
 
 # Add repository to APT sources.list
-echo deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/ | sudo tee --append /etc/apt/sources.list
+if [[ $(lsb_release -rs) == "20.04" ]]
+then
+    echo "Ubuntu 20.04 found"
+        if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" /etc/apt/sources.list
+        then
+            echo "R repo already in sources.list"
+        else
+            echo deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/ | sudo tee --append /etc/apt/sources.list
+        fi
+elif [[ $(lsb_release -rs) == "22.04" ]]
+then
+    echo "Ubuntu 22.04 found"
+        if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" /etc/apt/sources.list
+        then
+            echo "R repo already in sources.list"
+        else
+            echo deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/ | sudo tee --append /etc/apt/sources.list
+        fi
+elif [[ $(lsb_release -rs) == "24.04" ]]
+then
+    echo "Ubuntu 24.04 found"
+        if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/" /etc/apt/sources.list
+        then
+            echo "R repo already in sources.list"
+        else
+            echo deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/ | sudo tee --append /etc/apt/sources.list
+        fi
+else
+    echo "Non-compatible version"
+fi
 
-# Add keys
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
+# Add keys the proper way
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+
+# Add keys the workaround way (this didn't work when i tried it on 28/10/2024)
+# gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+# gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
 
 # Update repository list and install R
 sudo apt-get update && sudo apt-get install r-base r-base-dev -y
 
-# Install RStudio-Server
+# Install RStudio Server
 sudo apt-get install gdebi-core -y
-wget https://download2.rstudio.org/server/focal/amd64/rstudio-server-2023.09.1-494-amd64.deb
-sudo gdebi --non-interactive rstudio-server-2023.09.1-494-amd64.deb
-rm  rstudio-server-2023.09.1-494-amd64.deb
+if [[ $(lsb_release -rs) == "20.04" ]]
+then
+    wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+elif [[ $(lsb_release -rs) == "22.04" ]]
+then
+    wget https://www.rstudio.org/download/latest/stable/server/jammy/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+elif [[ $(lsb_release -rs) == "24.04" ]]
+then
+    wget https://www.rstudio.org/download/latest/stable/server/jammy/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+else
+    echo "Non-compatible version"
+fi
+sudo gdebi --non-interactive rstudio-latest.deb
+rm rstudio-latest.deb
 
 # Install nginx
 sudo apt-get install nginx -y
 
-# Configure nginx with RStudio-Server and Shiny-Server virtualhosts
-sudo wget https://raw.githubusercontent.com/jtsmith275/RStudioShiny-nginx/master/default -O /etc/nginx/sites-enabled/default
-
-# Install Ubuntu packages
-sudo apt-get update && sudo apt-get install \
-    gdebi-core \
-    pandoc \
-    pandoc-citeproc \
-    libcurl4-gnutls-dev \
-    libcairo2-dev \
-    libxt-dev \
-    libssl-dev \
-    libxml2-dev -y
-
-sudo apt-get update && sudo apt-get install \
-    lbzip2 \
-    libfftw3-dev \
-    libgdal-dev \
-    libgeos-dev \
-    libgsl0-dev \
-    libgl1-mesa-dev \
-    libglu1-mesa-dev \
-    libhdf4-alt-dev \
-    libhdf5-dev \
-    libjq-dev \
-    libpq-dev \
-    libproj-dev \
-    libprotobuf-dev \
-    libnetcdf-dev \
-    libsqlite3-dev \
-    libudunits2-dev \
-    netcdf-bin \
-    postgis \
-    protobuf-compiler \
-    sqlite3 \
-    tk-dev \
-    unixodbc-dev \
-    libv8-dev \
-    libnode-dev \
-    libcurl4-openssl-dev \
-    default-libmysqlclient-dev \
-    libmysqlclient-dev \
-    libssh2-1-dev -y
-
-# add addition system dependencies but suffixing \ <package name> on the end of the apt-get update & apt-get install -y command
+# Configure nginx with RStudio Server and Shiny Server virtualhosts
+sudo wget https://raw.githubusercontent.com/jb2cool/RStudioShiny-nginx/main/default -O /etc/nginx/sites-enabled/default
 
 # Install Shiny R package
-mkdir -p ~/R/x86_64-pc-linux-gnu-library/4.3
-R -e "install.packages('shiny', repos='https://cran.rstudio.com/', lib='~/R/x86_64-pc-linux-gnu-library/4.3')"
+mkdir -p ~/R/x86_64-pc-linux-gnu-library/4.4
+R -e "install.packages('shiny', repos='https://cran.rstudio.com/', lib='~/R/x86_64-pc-linux-gnu-library/4.4')"
 
-#Install other common R packages
-R -e "install.packages(c('directlabels','shinydashboard','ggplot2','plotly','scales','forcats','stringr','DT','readxl','tidyr','zoo','lubridate','reshape2','lemon','RColorBrewer','networkD3','shinyWidgets','shinyjs','shinycssloaders','openxlsx','readr','gcookbook','ggrepel','readODS','doBy','rtweet','httpuv','purrr','tm','wordcloud','jsonlite','lda','LDAvis','udpipe','lattice','tidytext','knitr','rmarkdown','readxl','htmltools','bs4Dash'),repos='https://cran.rstudio.com/', lib='~/R/x86_64-pc-linux-gnu-library/4.3')"
+# Install Shiny Server
+sudo apt-get install curl -y
+VERSION=$(curl https://download3.rstudio.org/ubuntu-18.04/x86_64/VERSION)
+wget --no-verbose "https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-$VERSION-amd64.deb" -O shiny-server-latest.deb
+sudo gdebi -n shiny-server-latest.deb
+rm shiny-server-latest.deb
 
-R -e "install.packages(c('tidyverse','dplyr','devtools','formatR','remotes','selectr','caTools','BiocManager',repos='https://cran.rstudio.com/', lib='~/R/x86_64-pc-linux-gnu-library/4.3'))"
-
-R -e "install.packages(c('RColorBrewer','RandomFields','RNetCDF','classInt','deldir','gstat','hdf5r','lidR','mapdata','maptools','mapview','ncdf4','proj4','raster','rgdal','rgeos','rlas','sf','sp','spacetime','spatstat','spdep','geoR','geosphere'), repos='http://r-forge.r-project.org/', lib='~/R/x86_64-pc-linux-gnu-library/4.3')"
-
-R -e "install.packages(c('forcats','shinycssloaders','odbc','fs','rlang','tibble','survey','stringr','survey','mosaic','DBI','fs','lubridate','magrittr','yaml','knitr','rmarkdown','testthat'), lib='~/R/x86_64-pc-linux-gnu-library/4.3')"
-
-R -e "install.packages(c('chron','countrycode','geojsonio','shinythemes','shinyTime','colourpicker','leaflet.minicharts','janitor','polite','rworldmap','shinyAce','treemap'), lib='~/R/x86_64-pc-linux-gnu-library/4.3')"
-
-R -e "install.packages(c('directlabels'), repos='http://r-forge.r-project.org/', lib='~/R/x86_64-pc-linux-gnu-library/4.3')" && rm -rf /tmp/downloaded_packages
-
-# Install Shiny-Server
-wget --no-verbose https://download3.rstudio.org/ubuntu-18.04/x86_64/VERSION -O "version.txt"
-VERSION=$(cat version.txt)
-wget --no-verbose "https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-$VERSION-amd64.deb" -O ss-latest.deb
-sudo gdebi -n ss-latest.deb
-rm -f version.txt ss-latest.deb
-
-# Configure Shiny-Server
-sudo wget https://raw.githubusercontent.com/jtsmith275/RStudioShiny-nginx/master/shiny-server.conf -O /etc/shiny-server/shiny-server.conf
+# Configure Shiny Server
 sudo sed -i "s/run_as shiny/run_as $USER/" /etc/shiny-server/shiny-server.conf
+sudo sed -i "s/3838;/ 3838 0.0.0.0;/" /etc/shiny-server/shiny-server.conf
 sudo sed -i "s/site_dir \/srv\/shiny-server/site_dir \/home\/$USER\/shiny/" /etc/shiny-server/shiny-server.conf
+if grep -q sanitize_errors /etc/shiny-server/shiny-server.conf
+then
+        echo "Additional Shiny config already completed"
+else
+    sudo sed -i '/directory_index on;$/a \ \ \ \ sanitize_errors off;\n \ \ \ disable_protocols xdr-streaming xhr-streaming iframe-eventsource iframe-htmlfile;' /etc/shiny-server/shiny-server.conf
+fi
 mkdir $HOME/shiny
 
 # Copy sample apps to users new Shiny dir
